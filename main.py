89dfa68ecb443519b9c8dcecc033a170674815cf
Pranjal Verma <pvcodes@gmail.com>
2026-02-20 16:27:24 +0530

    removed unwanted dep, added Dockerfile for scrapping job

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from scrapers_async.scraper import stats_scrapper as async_stats_scrapper
-from scrapers.scraper import stats_scrapper
 
 # from scrapers.scraper import stats_scrapper
 
 
 async def main():
-    # upload_bronze_data()
-    # list_all_local_data()
-    # return
 
     await async_stats_scrapper()
 
 
 if __name__ == "__main__":
-    stats_scrapper()
-    # asyncio.run(main())
+    asyncio.run(main())
